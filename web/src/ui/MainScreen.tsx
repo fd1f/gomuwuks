b2c966fe1182a027f5c4b9500e740592bf2f4a0b
plate <plate.email@proton.me>
2024-10-28 17:03:08 +0000

    allow alt+up/down to switch rooms (you must be in a room already)

--- web/src/ui/MainScreen.tsx
+++ web/src/ui/MainScreen.tsx
@@ -24,6 +24,7 @@ const MainScreen = () => {
 	const [activeRoomID, setActiveRoomID] = useState<RoomID | null>(null)
 	const client = use(ClientContext)!
 	const activeRoom = activeRoomID && client.store.rooms.get(activeRoomID)
+	const roomList = client.store.roomList
 	const setActiveRoom = useCallback((roomID: RoomID) => {
 		setActiveRoomID(roomID)
 		if (client.store.rooms.get(roomID)?.stateLoaded === false) {
@@ -33,7 +34,29 @@ const MainScreen = () => {
 	}, [client])
 	client.store.switchRoom = setActiveRoom
 	const clearActiveRoom = useCallback(() => setActiveRoomID(null), [])
-	return <main className={`matrix-main ${activeRoom ? "room-selected" : ""}`}>
+	const onKeyDownShortcuts = (evt: React.KeyboardEvent) => {
+		if (evt.altKey && (evt.key === "ArrowDown" || evt.key === "ArrowUp")) {
+			let direction = evt.key === "ArrowUp" ? 1 : -1 
+			// populate the list
+			let roomIDList: RoomID[] = []
+			roomList.current.forEach((e) => {roomIDList.push(e.room_id)})
+			// if no room is active, default to the last one in the array (top of the room list)
+			let nextIndex = roomIDList.length - 1
+			if (activeRoomID) {
+				nextIndex = roomIDList.indexOf(activeRoomID) + direction
+				// wraparound
+				if (nextIndex >= roomIDList.length) {
+					nextIndex = 0
+				} else if (nextIndex < 0) {
+					nextIndex = roomIDList.length - 1
+				}
+			}
+			console.log("we are currently on", activeRoomID)
+			console.log("this would switch to", roomIDList[nextIndex], roomList)
+			setActiveRoom(roomIDList[nextIndex])
+		}
+	}
+	return <main className={`matrix-main ${activeRoom ? "room-selected" : ""}`} onKeyDown={onKeyDownShortcuts}>
 		<RoomList setActiveRoom={setActiveRoom} activeRoomID={activeRoomID} />
 		{activeRoom && <RoomView key={activeRoomID} clearActiveRoom={clearActiveRoom} room={activeRoom} />}
 	</main>
