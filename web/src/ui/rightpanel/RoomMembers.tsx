6e238c5ef2f497356814d7bdc8fa88edb08a23fa
plate <plate.email@proton.me>
2024-11-09 15:27:50 +0000

    only show joined & invited members; add some css

--- web/src/ui/rightpanel/RoomMembers.tsx
+++ web/src/ui/rightpanel/RoomMembers.tsx
@@ -1,48 +1,84 @@
 import { use } from "react"
-//import { useRoomState } from "@/api/statestore"
 import { RoomContext } from "../roomview/roomcontext.ts"
-// this works for testing but i'll need to check with tulir that it's suitable
-import { getAutocompleteMemberList } from "../composer/userautocomplete"
+// is this even the right thing?
 import { getAvatarURL } from "@/api/media.ts"
-//import type { MemberEventContent } from "@/api/types"
-import type { UserID, ContentURI } from "@/api/types/mxtypes.ts"
+import type { UserID, ContentURI, MemberEventContent } from "@/api/types/mxtypes.ts"
+import { RoomStateStore } from "@/api/statestore/room.ts"
 
 
-// that might need to be an exported interface. what is an interface?? lol
-type RoomMemberEntryProps = {
-    userID: UserID,
-    avatarURL?: ContentURI,
-    displayname?: string, 
+export interface RoomMemberEntryProps {
+    userID: UserID
+    avatarURL?: ContentURI
+    displayName?: string
 }
 
+export interface RoomMemberCategoryProps {
+    title?: string
+    membership: string
+}
 
-// clean it up to match how components are usually made
+function getMemberList(room: RoomStateStore, membership: string) {
+    const members: RoomMemberEntryProps[] = []
+    const states = room.state.get("m.room.member")
+    console.log(
+        states,
+        room.eventsByRowID.get(room.state.get("m.room.power_levels")?.get("") ?? 0)
+    )
+    if (!states) {
+        return []
+    }
+    for (const[stateKey, rowID] of states) {
+        const memberEvt = room.eventsByRowID.get(rowID)
+        if (!memberEvt) {
+            continue
+        }
+        const content = memberEvt.content as MemberEventContent
+        if (content.membership == membership) {
+            members.push({
+                userID: stateKey,
+                avatarURL: content.avatar_url,
+                displayName: content.displayname
+            })
+        }
+    }
+    return members
+}
+
 const RoomMemberEntry = (props: RoomMemberEntryProps) => {
-    return (<div>
+    return (<div className="room-member">
         <img
             className="avatar"
             loading="lazy"
-            src={getAvatarURL(props.userID, {displayname: props.displayname, avatar_url: props.avatarURL})}
+            src={getAvatarURL(props.userID, {displayname: props.displayName, avatar_url: props.avatarURL})}
         />
-        {props.displayname ?? props.userID}
+        <span>{props.displayName ?? props.userID}</span>
     </div>)
 }
 
-const RoomMembers = () => {
+const RoomMemberCategory = (props: RoomMemberCategoryProps) => {
     const roomCtx = use(RoomContext)
     if (!roomCtx) {
         return null
     }
-    const members = getAutocompleteMemberList(roomCtx.store)
+    const members = getMemberList(roomCtx.store, props.membership)
     console.log(members)
-    let list = Array()
-    members.forEach((member) => {
-        list.push(
-            // lol `?? "" is to shut typescript warnings up. need to look into that
-            <RoomMemberEntry userID={member.userID} avatarURL={member.avatarURL} displayname={member.displayName}/>
-        )
-    })
-    return <ul>{list}</ul>
+    let list = members.map(member => 
+        <RoomMemberEntry userID={member.userID} avatarURL={member.avatarURL} displayName={member.displayName}/>
+    )
+    if (list.length === 0) {
+        return null
+    }
+    return <>
+        {props.title ? <p>{props.title}</p> : null}
+        <div className="member-list">{list}</div>
+    </>
+}
+
+const RoomMembers = () => {
+    return <>
+        <RoomMemberCategory membership="join" />
+        <RoomMemberCategory membership="invite" title="Invited" />
+    </>
 }
 
 export default RoomMembers
